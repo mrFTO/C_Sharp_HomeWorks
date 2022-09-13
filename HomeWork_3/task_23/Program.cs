bf2883a2c042605c8cb7979a01c41ab3a6a1493e
mrFTO <ntoon@ya.ru>
2022-09-13 18:28:17 +0300

    Finished task_23

---
+++ HomeWork_3/task_23/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Please, enter your number: "); //Запрашиваем число у пользователя.
+int num = Convert.ToInt32(Console.ReadLine()); // Присваиваем введенное значение переменной num и преобразуем его в числовое значение.
+
+void Koob(int[] num) // Создаем метод для расчета куба числа.
+{
+    int counter = 0;
+    int length = num.Length;
+    while (counter < length)
+    {
+        num[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+        counter++;
+    }
+}
+
+void PrintArray(int[] coub) // Создаем метод для отображения массива с расчетными кубами числа.
+{
+    int count = coub.Length;
+    int index = 1;
+    while (index < count)
+    {
+        Console.Write(coub[index] + " ");
+        index++;
+    }
+}
+
+int[] arr = new int[num + 1]; //Создаем массив.
+Koob(arr); //Вызываем метод расчета кубов.
+PrintArray(arr); // Вызываем метод отображения массива.
