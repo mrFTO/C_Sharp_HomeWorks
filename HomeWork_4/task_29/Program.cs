30b46c2cfcc1a9ed6ab94727bb5bc5ce0202657f
mrFTO <ntoon@ya.ru>
2022-09-16 17:26:19 +0300

    Finished task_29.

---
+++ HomeWork_4/task_29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+int[] numbers = new int[8]; // Создаем массив из 8 элементов и заполняем его случайными числами от 0 до 10.
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 11);
+    Console.Write(" " + Method(i) + " ");
+}
+Console.Write("]");
+
+int Method(int a)
+{
+    return numbers[a];
+}
+
