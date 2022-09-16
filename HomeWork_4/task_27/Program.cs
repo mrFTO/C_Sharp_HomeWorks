c107cc88264252c3022c5e235c733f5f3b86bd22
mrFTO <ntoon@ya.ru>
2022-09-16 16:34:30 +0300

    Finished task_27.

---
+++ HomeWork_4/task_27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Please enter your number: "); // Запрашиваем у пользователя любое число.
+int num = Convert.ToInt32(Console.ReadLine()); // Присваиваем введенное значение переменной num и преобразуем его в числовое значение.
+int sum = 0;
+// Создаем метод для расчета суммы цифр числа.
+int Summ(int num)
+{
+    while (num > 0)
+    {
+        int dig = num % 10;
+        num = num / 10;
+        sum = sum + dig;
+    }
+    Console.WriteLine("The sum of all digits of a number is " + sum);
+    return sum;
+}
+// Вызываем метод для указанного пользователем числа.
+Summ(num);
